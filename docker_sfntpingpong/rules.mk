58b465b9d58d40fb3b2efd2089ee3d693d82824c
venkatvandy <venkataramana.nagarajan@vanderbilt.edu>
2017-12-14 01:58:59 -0600

    Code files

---
+++ docker_sfntpingpong/rules.mk
@@ -0,0 +1,23 @@
+
+#CFLAGS		+= -Wall -Werror
+CFLAGS         += -Wall
+
+ifdef OS_LINUX
+# Add symbols to dynamic symbol table.  This is necessary to allow
+# detection of onload in sfnt_sysinfo.c
+CCLINKFLAGS	+= -Wl,-E
+else
+CCLINKFLAGS	+= -Wl
+endif
+
+
+# Disable built-in rules.
+%: %.c
+%: %.o
+
+%.a:
+	$(AR) -r $@ $?
+
+# Build app from object file with same name.
+%: %.o
+	$(CC) $(CCLINKFLAGS) $(CFLAGS) $< $(LIBS) -o $@
